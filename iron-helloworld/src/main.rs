e15cb7b00b2b530d604831c8fdf71f70f7445472
MURAKI Taichi <muraki.taichi@gmail.com>
2020-12-31 13:16:01 +0900

    Add helloworld of http server using by iron

---
+++ iron-helloworld/src/main.rs
@@ -0,0 +1,7 @@
+use iron::prelude::*;
+use iron::status;
+
+fn main() {
+    Iron::new(|_: &mut Request| Ok(Response::with((status::Status::Ok, "Hello world!"))))
+        .http("localhost:3000").unwrap();
+}
