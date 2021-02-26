8475b6bf7eb43ac94f1a7ad39fd45b41cec97382
MURAKI Taichi <muraki.taichi@gmail.com>
2021-02-26 14:57:58 +0900

    文字列に関する学習

--- study-string/src/main.rs
+++ study-string/src/main.rs
@@ -1,9 +1,15 @@
-fn set_hello_world(str: &mut String) {
-    str.push_str("Hello World.");
+fn adder(x: i32, y: i32) -> i32 {
+    x + y
+}
+
+fn set_hello_world(str: &mut String, num: i32) {
+    let ans = adder(5, num);
+    let s = format!("Hello World: ans={}", ans);
+    str.push_str(&s);
 }
 
 fn main() {
     let mut s = "".to_string();
-    set_hello_world(&mut s);
+    set_hello_world(&mut s, 3);
     println!("{}", s);
 }
