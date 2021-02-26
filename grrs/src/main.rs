8cd30721fa7743132bba758c0ce023614c3d8040
MURAKI Taichi <muraki.taichi@gmail.com>
2021-02-26 16:43:00 +0900

    fix

--- grrs/src/main.rs
+++ grrs/src/main.rs
@@ -8,14 +8,11 @@ struct Cli {
 }
 
 fn main() {
-    // let pattern = std::env::args().nth(1).expect("no pattern given");
-    // let path = std::env::args().nth(2).expect("no path given");
-    // let args = Cli {
-    //     pattern: pattern,
-    //     path: std::path::PathBuf::from(path),
-    // };
     let args = Cli::from_args();
-    let content = std::fs::read_to_string(&args.path)
-    .expect("could not read file");
-    println!("Hello, world! {}", content);
+    let content = std::fs::read_to_string(&args.path).expect("could not read file");
+    for line in content.lines() {
+        if line.contains(&args.pattern) {
+            println!("{}", line);
+        }
+    }
 }
