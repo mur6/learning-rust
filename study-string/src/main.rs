a38302118a6acf1959b4ea20b7b9187a12382945
MURAKI Taichi <muraki.taichi@gmail.com>
2021-04-29 16:11:16 +0900

    Fix sample code to study string.

--- study-string/src/main.rs
+++ study-string/src/main.rs
@@ -12,4 +12,22 @@ fn main() {
     let mut s = "".to_string();
     set_hello_world(&mut s, 3);
     println!("{}", s);
+    // 1.
+    let s = "abc";
+    println!("{}", s);
+    // 2. concat
+    let hello = "hello".to_string();
+    let world = "world";
+    let s2 = hello + world;
+    println!("{}", s2);
+    // 3. 比較
+    if s == "abc" {
+        println!("同じ!");
+    }
+    // 4. 文字列フォーマット
+    // 5. join
+    //let vs = vec!["Foo".to_string(),"bar".to_string(), "baz".to_string()];
+    let vs = vec!["foo", "bar", "baz"];
+    let s3 = vs.join("-");
+    println!("{}", s3);
 }
